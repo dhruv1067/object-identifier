5ea9caebfbe1adf2124395b633d12230b7339f17
dhruv1067 <dhruv67d@gmail.com>
2025-07-21 11:55:29 +0530

    command line args added and minor changes

--- object.py
+++ object.py
@@ -1,19 +1,26 @@
 import cv2
 import numpy as np    
+
+# Load YOLOv4-Tiny Model
 net = cv2.dnn.readNet("yolov4-tiny.weights" , "yolov4-tiny.cfg")
+# Load COCO class labels
 with open("coco.names", "r") as f:classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers().flatten() ]
+# Video Source (IP camera)
 cap = cv2.VideoCapture('http://192.168.77.207:8080/video')
+# Settings
 frame_skip = 5
 frame_id = 0
 colors = np.random.uniform(0 , 255 , size=(len(classes) , 3 ))
+# Main Loop
 while True:
     ret, frame = cap.read()
     if not ret : break
     start = cv2.getTickCount()
     frame_id += 1 
     if frame_id % frame_skip != 0 : continue
+    # Pre-processes frame for YOLO
     small_frame = cv2.resize(frame, (320,320))
     blob = cv2.dnn.blobFromImage(small_frame , 1/255.0 , (416, 416) , swapRB=True)
     net.setInput(blob)
@@ -36,12 +43,15 @@ while True:
             label = f"{classes[class_ids[i]]}: {confidences[i]: .2f}"
             color = colors[class_ids[i]]
             cv2.rectangle(frame, (x,y), (x + w, y + h), color, 2)
-            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+    # Calculate FPS
     end = cv2.getTickCount()
     fps = cv2.getTickFrequency() / (end - start)
     cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), 
     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-    cv2.imshow("YOLOv4 Detection", cv2.resize(frame, (2200, 860)))
+    # Resize Dynamically
+    display_frames = cv2.resize(frame, (1600, 800))
+    cv2.imshow("YOLOv4 Detection", display_frames)
     if cv2.waitKey(1) == ord('q'): break
 cap.release()
 cv2.destroyAllWindows()
