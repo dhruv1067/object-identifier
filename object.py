5a0a0290652061298e363e1be8464efb0ef7d69e
dhruv1067 <dhruv67d@gmail.com>
2025-08-11 22:12:14 +0530

    added one

--- object.py
+++ object.py
@@ -27,28 +27,7 @@ while True:
     outputs = net.forward(output_layers)
     boxes, confidences , class_ids = [] , [] , []
     for output in outputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id =  np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                x, y, w, h = map(int , detection[:4] * [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-                boxes.append([x - w // 2, y - h // 2 , w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    if len(indexes) > 0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            label = f"{classes[class_ids[i]]}: {confidences[i]: .2f}"
-            color = colors[class_ids[i]]
-            cv2.rectangle(frame, (x,y), (x + w, y + h), color, 2)
-            cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-    # Calculate FPS
-    end = cv2.getTickCount()
-    fps = cv2.getTickFrequency() / (end - start)
-    cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), 
-    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        
     # Resize Dynamically
     display_frames = cv2.resize(frame, (1200, 800))
     cv2.imshow("YOLOv4 Detection", display_frames)
